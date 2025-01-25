eb5cf775c0d796a82577533d215abc14bd961cd7
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2025-01-25 07:08:15 -0500

    feat: agregar soporte para dotenv y cargar variables de entorno en modo desarrollo

--- src/infrastructure/di/container.ts
+++ src/infrastructure/di/container.ts
@@ -1,4 +1,6 @@
 import 'reflect-metadata';
+import * as dotenv from 'dotenv';
+
 import { container } from 'tsyringe';
 
 import { IAppConfig } from '../../domain/config/IAppConfig';
@@ -9,6 +11,11 @@ import { NasaApodRepository } from '../repositories/NasaApodRepository';
 
 import { ISwapiPeopleRepository } from '../../domain/repositories/ISwapiPeopleRepository';
 import { SwapiPeopleRepository } from '../repositories/SwapiPeopleRepository';
+
+if (process.env.NODE_ENV !== 'production') {
+	dotenv.config();
+	console.log('Environment variables loaded in development mode...');
+}
 
 // 1) Registro de AppConfig como "AppConfig"
 container.register<IAppConfig>('AppConfig', {
